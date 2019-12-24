b0e800f4e7e6c0626f4de8bfb382ed792ae04402
MxmStm <you@example.com>
2019-12-24 21:06:07 +0300

    Added test add/remove button

---
+++ src/test/java/HerokuAppTest.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class HerokuAppTest {
+
+    @Test
+    public void addRemoveElements(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.tagName("button")).click();
+        driver.findElements(By.className("added-manually")).get(0).click();
+        int numberOfDeleteButtons = driver.findElements(By.className("added-manually")).size();
+        assertEquals(numberOfDeleteButtons,1,"Number of elements is not correct");
+
+        driver.quit();
+    }
+}
